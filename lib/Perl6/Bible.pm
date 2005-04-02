e617e0961672edb4082cf864c3faf07d7b098c6b
Ingy döt Net <ingy@cpan.org>
2005-04-02 07:19:44 +0000

    Import of INGY/Perl6-Bible-0.10 from CPAN.
    gitpan-cpan-distribution: Perl6-Bible
    gitpan-cpan-version:      0.10
    gitpan-cpan-path:         INGY/Perl6-Bible-0.10.tar.gz
    gitpan-cpan-author:       INGY
    gitpan-cpan-maturity:     released

---
+++ lib/Perl6/Bible.pm
@@ -0,0 +1,37 @@
+package Perl6::Bible;
+use strict;
+use warnings;
+
+our $VERSION = '0.10';
+
+1;
+
+__DATA__
+
+=head1 NAME
+
+Perl6::Bible - The Gospel according to Cabal
+
+=head1 SYNOPSIS
+
+    > p6bible s29
+
+=head1 DESCRIPTION
+
+This distribution contains all the latest Perl6 documentation and a utility
+called C<p6bible> for viewing them. 
+
+=head1 AUTHOR
+
+Brian Ingerson <ingy@cpan.org>
+
+=head1 COPYRIGHT
+
+Copyright (c) 2005. Brian Ingerson. All rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+See http://www.perl.com/perl/misc/Artistic.html
+
+=cut
---
+++ t/compile.t
@@ -0,0 +1,7 @@
+use lib 't', 'lib';
+use strict;
+use warnings;
+use Test::More tests => 1;
+use Perl6::Bible;
+
+ok(1);
